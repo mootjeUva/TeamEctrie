2173085e8e55031f5be44137e0501a6f69550cfe
Rowan Schelvis <schelvisrowan@gmail.com>
2023-01-19 14:00:43 +0100

    comments toegevoegd aan randomise2.py

--- code/algorithms/randomise2.py
+++ code/algorithms/randomise2.py
@@ -18,19 +18,33 @@ def random_algoritme2(graph: Graph, traject: Traject) -> None:
 
     # Select random starting station
     start_station = random.choice(list(graph.connections.keys()))
+
+    # Set starting distance equal to 0
     distance = 0
+
+    # Add starting station to traject and save the connections
     traject.add_station(start_station, distance)
     connections = graph.connections[start_station]
     tmp = start_station
 
     while True:
+        
+        # Find connection key list
         ck_list = list(connections.keys())
+
+        # Check if endpoint reached
         if len(ck_list) < 1:
             break
+        
+        # Pick a random station and find the distance to it
         next_station = random.choice(ck_list)
         distance = connections[next_station]
+        
+        # Check if the next station can be reached within the timeframe
         if (traject.total_distance + distance) > 120:
             break
+        
+        # Add connections and station to the trajrect
         con = Connection(tmp, next_station, distance)
         con2 = Connection(next_station, tmp, distance)
         traject.add_connection(con)
